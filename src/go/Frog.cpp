b22cd65a942c539c0ddf5d10941865e26b169a23
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2014-09-28 16:31:24 +0100

    Add
    Add new files to Frog file.

--- src/go/Frog.cpp
+++ src/go/Frog.cpp
@@ -1,6 +1,48 @@
-class Frog: public DynamicObject{
+#include "Frog.h"
+
+Frog::Frog()
+{
+	
+}
+
+class Frog: public DynamicObject
+{
 	public:	
 		Frog();
 		~Frog();
 		void draw();
+		
+	/*
+	 *
+	 *	New changes by Francisco Maria Calisto
+	 *
+	 */
+
+	void Frog::drawModel()
+	{
+		if (!pmodel)
+		{
+			pmodel = glmReadOBJ("ship.obj");
+			if (!pmodel) exit(0);
+			glmUnitize(pmodel);
+			glmFacetNormals(pmodel);
+			glmVertexNormals(pmodel, 90.0);
+		}
+		glmDraw(pmodel, GLM_SMOOTH );
+	}
+
+	void Frog::draw()
+	{
+		drawModel();
+		/*
+		glPushMatrix();		
+			glTranslatef(_transform.getX(), _transform.getY(), _transform.getZ());
+			glRotatef(_rotate.getX(), 1, 0, 0);
+			glRotatef(_rotate.getY(), 0, 1, 0);
+			glRotatef(_rotate.getZ(), 0, 0, 1);
+			glScalef(_scale.getX(), _scale.getY(), _scale.getZ());
+			drawModel();
+		glPopMatrix();
+		*/
+	}
 };
