4cb8e3509793034cf1999e611f37f2edca2c464d
FMCalisto <francisco_mcalisto@hotmail.com>
2014-10-11 19:12:12 +0100

    Update
    Update file with the Xcode solution root directory.

---
+++ Xcode solution root/Xcode solution root/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Xcode solution root
+//
+//  Created by Francisco Maria Calisto on 11/10/14.
+//  Copyright (c) 2014 Francisco Maria Calisto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
